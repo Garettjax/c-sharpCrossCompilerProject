efb4dadf042fc67f764646ac8933fd4196ea2aeb
Mohsin <mmkabir031@gmail.com>
2020-09-19 23:38:20 -0400

    Push customParser

--- src/CustomParser.java
+++ src/CustomParser.java
@@ -1,3 +1,8 @@
+//TODO: issues to address Relational Operators, especially 2 char ones, we arent supporting objects so make "Console.WriteLine" as one thing
+
+
+
+
 import java.util.ArrayList;
 
 public class CustomParser {
@@ -32,86 +37,123 @@ public class CustomParser {
 		if(self.currentLine == 1) {
 			moveForward(self);
 		}
-		
 		//Keep going till the end of the file
 		while(!self.endOfFile(self)) {
-			
 			//self explanatory functions 
 			removeComments(self);
 			removeWhitespace(self);
-			
 			//If current character is a symbol then it's a token
 			//Let's add the token
 			if(containsSymbol(self.currentChar)) {
-				switch (self.currentChar) {
-				case '(':
-					tokens.add(new Token("(", "LEFT_PAREN", self.currentLine));
-					//tokens.add("Left paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case ')':
-					tokens.add(new Token(")", "RIGHT_PAREN", self.currentLine));
-					//tokens.add("Right paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '[':
-					tokens.add(new Token("[", "LEFT_BRACKET", self.currentLine));
-					//tokens.add("Left bracket, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case ']':
-					tokens.add(new Token("]", "RIGHT_BRACKET", self.currentLine));
-					//tokens.add("Right bracket, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '+':
-					tokens.add(new Token("+", "ADDITION_OP", self.currentLine));
-					//tokens.add("Addition, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '-':
-					tokens.add(new Token("-", "SUBTRACTION_OP", self.currentLine));
-					//tokens.add("Subtraction Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '\\':
-					tokens.add(new Token("\\", "DIVISION_OP", self.currentLine));
-					//tokens.add("Division Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '*':
-					tokens.add(new Token("*", "MULTIPLICATION_OP", self.currentLine));
-					//tokens.add("Multiplication Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case ',':
-					tokens.add(new Token(",", "COMMA", self.currentLine));
-					//tokens.add("Comma, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case ';':
-					tokens.add(new Token(";", "SEMICOLON", self.currentLine));
-					//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '{':
-					tokens.add(new Token("{", "LEFT_CURLY", self.currentLine));
-					//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
-				case '}':
-					tokens.add(new Token("}", "RIGHT_CURLY", self.currentLine));
-					//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
-					break;
+				for (EnumToken EnumTokenType : EnumToken.values()) {
+					if (EnumTokenType.getChar() == (self.currentChar)) {
+						switch (EnumTokenType) {
+							//  LE_OPERATOR(''
+							//LT_OPERATOR('
+							// GE_OPERATOR(''
+							//case	GT_OPERATOR:
+							// EQ_OPERATOR(''
+							// NE_OPERATOR(''
+
+
+							//TODO: check if its possible to use "lex" from enum cuz it is there.... will look into it in a while
+							//Done
+
+							case ASSIG://TODO: confirm if this is not a token? aka "="
+
+
+
+
+
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()) , "ASSIGN_OP", self.currentLine));
+
+								//tokens.add("Left paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case MINUS:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "SUBTRACTION_OP", self.currentLine));
+								//tokens.add("Subtraction Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case MULTIPLY:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "MULTIPLICATION_OP", self.currentLine));
+								//tokens.add("Multiplication Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case DIVIDE:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "DIVISION_OP", self.currentLine));
+								//tokens.add("Division Op, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case PLUS:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "ADDITION_OP", self.currentLine));
+								//tokens.add("Addition, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case SEMICOLON:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "SEMICOLON", self.currentLine));
+								//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+
+							case MOD:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "MODULUS ", self.currentLine));
+								//tokens.add("Left paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+
+							case OPEN_BRACKET:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "LEFT_PAREN", self.currentLine));
+								//tokens.add("Left paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+
+							case CLOSE_BRACKET:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "RIGHT_PAREN", self.currentLine));
+								//tokens.add("Right paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case EXPONENT:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "EXPONENT", self.currentLine));
+								//tokens.add("Left paren, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case COMMA:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "COMMA", self.currentLine));
+								//tokens.add("Comma, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case SQUARE_OPEN_BRACKET:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "LEFT_BRACKET", self.currentLine));
+								//tokens.add("Left bracket, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case SQUARE_CLOSE_BRACKET:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "RIGHT_BRACKET", self.currentLine));
+								//tokens.add("Right bracket, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case CURLY_BRACKET_OPEN:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "LEFT_CURLY", self.currentLine));
+								//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+							case CURLY_BRACKET_CLOSE:
+								tokens.add(new Token(Character.toString(EnumTokenType.getChar()), "RIGHT_CURLY", self.currentLine));
+								//tokens.add("Semicolon, symbol: " + self.currentChar + ", Line #" + self.currentLine);
+								break;
+						}
+					}
 				}
-				
-				
 				self.moveForward(self);
 			}
+
+
+
+
+
 			//Let's now check for stringLiteral's
 			token = stringLiteral(self);
-			
+
+
+
 			//If we got one we add it, if not move on
+			//TODO: get what? give an example perhaps?
 			if (token.getLexeme() != "") {
 				tokens.add(token);
 				//token = "";
 			}
 			//Stops at the first sign of whitespace, a special symbol like addition '+', or if we reached the end of the file
 			while(!Character.isWhitespace(self.currentChar) && self.currentChar != '\0' && !containsSymbol(self.currentChar)) {
-				
 				removeComments(self);
 				removeWhitespace(self);
 				removeComments(self);
-				
 				tempToken += self.currentChar;
 				self.moveForward(self);
 			}
@@ -122,7 +164,6 @@ public class CustomParser {
 				tempToken = "";
 				//tokens.add("Reserved Word, symbol: " + token + ", Line #" + self.currentLine);
 			}
-			
 			else if (tempToken != "") {
 				//Look for integer literal using regular expressions
 				if (tempToken.matches("[0-9]+")) {
@@ -166,7 +207,6 @@ public class CustomParser {
 	//Check for and return tokens of type string literal
 	public Token stringLiteral (CustomParser self) {
 		String result = "";
-		
 		if (self.currentChar != '\"') {
 			return new Token(null, null, -1);
 		}
@@ -212,7 +252,6 @@ public class CustomParser {
 		}
 		return result;
 	}
-	
 	//Move forward one character
 	public void moveForward(CustomParser self) {
 		if(self.lastChar == '\n') {
@@ -238,44 +277,25 @@ public class CustomParser {
 			}
 		}
 	}
-	
 	//Check reserved words list
 	public boolean isReservedWord(String value) {
+
+		//TODO: make sure it uses ENUMKeywords
+		//Done
+
 		boolean reserved = false;
 
-		ArrayList<String> reservedWordsList = new ArrayList<String>();
-		reservedWordsList.add("using");
-		reservedWordsList.add("System");
-		reservedWordsList.add("namespace");
-		reservedWordsList.add("internal");
-		reservedWordsList.add("class");
-		reservedWordsList.add("public");
-		reservedWordsList.add("static");
-		reservedWordsList.add("void");
-		reservedWordsList.add("Main");
-		reservedWordsList.add("string");
-		reservedWordsList.add("constant");
-		reservedWordsList.add("begin");
-		reservedWordsList.add("if");
-		reservedWordsList.add("else");
-		reservedWordsList.add("elseIf");
-		reservedWordsList.add("until");
-		reservedWordsList.add("return");
-		reservedWordsList.add("int");
-		reservedWordsList.add("double");
-		reservedWordsList.add("while");
-		reservedWordsList.add("do");
-		reservedWordsList.add("global");
-		reservedWordsList.add("for");
-		
-		if(reservedWordsList.contains(value)) {
-			reserved = true;
+		for (EnumKeyword m : EnumKeyword.values()) {
+			if(m.getString().equals(value)){
+				reserved = true;
+				//System.out.println("Enum:  " + m.getString());
+				//System.out.println("Value: " + value);
+			}
 		}
-		else {
-			reserved = false;
-		}
+		//reserved is always false UNLESS above if statement is true
 		return reserved;
 	}
+	//TODO: why was this made? ?
 	public Token addReservedWord(String value) {
 		value = value.toLowerCase();
 		if(value.equalsIgnoreCase("NOT") ||
@@ -288,29 +308,24 @@ public class CustomParser {
 	}
 	
 	//Check if character is a special symbol
+
+	//TODO: Ask brett if we want to change this to String cuz of "==" or "!=" etc
+	//TODO: for now i have commented them out
+
 	public boolean containsSymbol(char ch) {
+		//TODO: make sure it uses ENUMToken
+		//Done
 		boolean isSymbol = false;
-		
-		ArrayList<Character> symbolList = new ArrayList<Character>();
-		symbolList.add('+');
-		symbolList.add('-');
-		symbolList.add('*');
-		symbolList.add(',');
-		symbolList.add('(');
-		symbolList.add(')');
-		symbolList.add('[');
-		symbolList.add(']');
-		symbolList.add(';');
-		symbolList.add('{');
-		symbolList.add('}');
-		
-		if(symbolList.contains(ch)) {
-			isSymbol = true;
+
+		for (EnumToken n : EnumToken.values()) {
+			if (n.getChar() == (ch)) {
+				isSymbol = true;
+				//System.out.println("Enum:  " + n.getCharacters());
+				//System.out.println("Value: " + ch);
+			}
 		}
-		
 		return isSymbol;
 	}
-
 	//Check if end of file
 	public boolean endOfFile(CustomParser self) {
 		if(self.currentChar == '\0') {
